4296ac64b6d244bc89acf82b25ac0575f2cb5e4a
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-08-09 11:34:12 +0000

    initial upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jh_magnificpopup/trunk@78935 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "jh_magnificpopup".
+ *
+ * Auto generated 17-07-2013 20:32
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Magnific Popup',
+	'description' => 'Open an image with the responsive Magnific Popup jQuery lightbox plugin.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Jonathan Heilmann',
+	'author_email' => 'mail@jonathan-heilmann.de',
+	'author_company' => '',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'typo3' => '4.5.0-6.1.99',
+		),
+		'conflicts' => '',
+		'suggests' =>
+		array (
+		),
+	),
+);
+
+?>
