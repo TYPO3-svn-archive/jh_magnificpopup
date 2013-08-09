fef9ef364d3acbe28e6eef1e9b1fe8c6ae463fe6
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-08-09 17:37:24 +0000

    Updated ext_emconf.php after release of version 0.0.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jh_magnificpopup/trunk@78945 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "jh_magnificpopup".
  *
- * Auto generated 09-08-2013 13:41
+ * Auto generated 09-08-2013 19:35
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -12,10 +12,10 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Magnific Popup',
-	'description' => 'Open an image with the responsive Magnific Popup jQuery lightbox plugin.',
+	'description' => 'Open images with the responsive Magnific Popup jQuery lightbox plugin.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.0.1',
+	'version' => '0.0.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:17:"ext_localconf.php";s:4:"8d5c";s:14:"ext_tables.php";s:4:"8c3a";s:10:"Readme.rst";s:4:"d41d";s:38:"Configuration/TypoScript/constants.txt";s:4:"10dd";s:34:"Configuration/TypoScript/setup.txt";s:4:"4dc2";s:40:"Resources/Private/Language/locallang.xml";s:4:"7ec6";s:39:"Resources/Public/css/magnific-popup.css";s:4:"6bfa";s:40:"Resources/Public/js/jquery-1.10.2.min.js";s:4:"841d";s:39:"Resources/Public/js/jquery-2.0.3.min.js";s:4:"1034";s:48:"Resources/Public/js/jquery.magnific-popup.min.js";s:4:"3db9";s:14:"doc/manual.sxw";s:4:"3ad8";}',
+	'_md5_values_when_last_written' => 'a:12:{s:12:"ext_icon.png";s:4:"8539";s:17:"ext_localconf.php";s:4:"8d5c";s:14:"ext_tables.php";s:4:"8c3a";s:10:"Readme.rst";s:4:"d41d";s:38:"Configuration/TypoScript/constants.txt";s:4:"5934";s:34:"Configuration/TypoScript/setup.txt";s:4:"b319";s:40:"Resources/Private/Language/locallang.xml";s:4:"7ec6";s:39:"Resources/Public/css/magnific-popup.css";s:4:"6bfa";s:40:"Resources/Public/js/jquery-1.10.2.min.js";s:4:"841d";s:39:"Resources/Public/js/jquery-2.0.3.min.js";s:4:"1034";s:48:"Resources/Public/js/jquery.magnific-popup.min.js";s:4:"3db9";s:14:"doc/manual.sxw";s:4:"bc43";}',
 );
 
 ?>
