ab2c23525f5fff3707fa698fb6a626d50b359c92
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-08-09 11:42:53 +0000

    updated ext_emconf.php after initial release (version 0.0.1)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jh_magnificpopup/trunk@78936 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,25 +3,25 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "jh_magnificpopup".
  *
- * Auto generated 17-07-2013 20:32
+ * Auto generated 09-08-2013 13:41
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'Magnific Popup',
 	'description' => 'Open an image with the responsive Magnific Popup jQuery lightbox plugin.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
@@ -30,19 +30,20 @@ $EM_CONF[$_EXTKEY] = array (
 	'author' => 'Jonathan Heilmann',
 	'author_email' => 'mail@jonathan-heilmann.de',
 	'author_company' => '',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
-	'constraints' =>
-	array (
-		'depends' =>
-		array (
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
 			'typo3' => '4.5.0-6.1.99',
 		),
-		'conflicts' => '',
-		'suggests' =>
-		array (
+		'conflicts' => array(
+		),
+		'suggests' => array(
 		),
 	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:11:{s:17:"ext_localconf.php";s:4:"8d5c";s:14:"ext_tables.php";s:4:"8c3a";s:10:"Readme.rst";s:4:"d41d";s:38:"Configuration/TypoScript/constants.txt";s:4:"10dd";s:34:"Configuration/TypoScript/setup.txt";s:4:"4dc2";s:40:"Resources/Private/Language/locallang.xml";s:4:"7ec6";s:39:"Resources/Public/css/magnific-popup.css";s:4:"6bfa";s:40:"Resources/Public/js/jquery-1.10.2.min.js";s:4:"841d";s:39:"Resources/Public/js/jquery-2.0.3.min.js";s:4:"1034";s:48:"Resources/Public/js/jquery.magnific-popup.min.js";s:4:"3db9";s:14:"doc/manual.sxw";s:4:"3ad8";}',
 );
 
 ?>
