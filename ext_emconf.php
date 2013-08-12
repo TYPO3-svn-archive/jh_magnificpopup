8c47f386fe06bfe2d3680463c49ed20273320c5f
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-08-12 15:53:31 +0000

    Updated ext_emconf.php for release of version 0.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jh_magnificpopup/trunk@78985 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Open images with the responsive Magnific Popup jQuery lightbox plugin.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.0.2',
+	'version' => '0.1.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:12:"ext_icon.png";s:4:"8539";s:17:"ext_localconf.php";s:4:"8d5c";s:14:"ext_tables.php";s:4:"8c3a";s:10:"Readme.rst";s:4:"d41d";s:38:"Configuration/TypoScript/constants.txt";s:4:"5934";s:34:"Configuration/TypoScript/setup.txt";s:4:"b319";s:40:"Resources/Private/Language/locallang.xml";s:4:"7ec6";s:39:"Resources/Public/css/magnific-popup.css";s:4:"6bfa";s:40:"Resources/Public/js/jquery-1.10.2.min.js";s:4:"841d";s:39:"Resources/Public/js/jquery-2.0.3.min.js";s:4:"1034";s:48:"Resources/Public/js/jquery.magnific-popup.min.js";s:4:"3db9";s:14:"doc/manual.sxw";s:4:"bc43";}',
+	'_md5_values_when_last_written' => '',
 );
 
 ?>
