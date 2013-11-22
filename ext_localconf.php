c52a1e60b0327adb2178cde37fd8f51dfa6497b0
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-22 14:53:30 +0000

    Added iframe to RTE (via Page TSconfig)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jh_magnificpopup/trunk@81698 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -16,4 +16,33 @@ if (!defined('TYPO3_MODE')) {
 
 // Save the IRRE content (use hook to change colPos)
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_jhmagnificpopup_tcemain.php:&tx_jhmagnificpopup_tcemain';
+
+// Page TSconfig for global enabled iframe
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+	RTE.classesAnchor {
+	  externalLinkInMagnificpopup {
+	    class = mfp-link external-link-new-window
+	    type = url
+	    image >
+	    titleText = LLL:EXT:jh_magnificpopup/Resources/Private/Language/locallang.xml:pageTSconfig.externalLinkInMagnificpopup
+	  }
+	  internalLinkInMagnificpopup {
+	    class = mfp-link internal-link-new-window
+	    type = page
+	    image >
+	    titleText = LLL:EXT:jh_magnificpopup/Resources/Private/Language/locallang.xml:pageTSconfig.internalLinkInMagnificpopup
+	  }
+	  downloadInMagnificpopup {
+	    class = mfp-link download
+	    type = file
+	    image >
+	    titleText = LLL:EXT:jh_magnificpopup/Resources/Private/Language/locallang.xml:pageTSconfig.downloadInMagnificpopup
+	  }
+	}
+
+	RTE.default.proc.allowedClasses := addToList(mfp-link external-link-new-window, mfp-link internal-link-new-window, mfp-link download)
+	RTE.default.classesAnchor := addToList(mfp-link external-link-new-window, mfp-link internal-link-new-window, mfp-link download)
+
+	RTE.default.buttons.link.properties.class.allowedClasses := addToList(mfp-link external-link-new-window, mfp-link internal-link-new-window, mfp-link download)
+');
 ?>
