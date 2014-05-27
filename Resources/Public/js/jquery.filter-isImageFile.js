1b1633b6cc534debff1064bfca361a6d2c271e9e
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-05-27 17:10:35 +0000

    removed jQuery.noConflict for isImageFile-filter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jh_magnificpopup/trunk@85402 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/js/jquery.filter-isImageFile.js
+++ Resources/Public/js/jquery.filter-isImageFile.js
@@ -1,14 +1,15 @@
-/*! jQuery :isImageFile filter - v0.0.2 - 2014-02-23
+/*! jQuery :isImageFile filter - v0.0.3 - 2014-05-27
 *
 * Copyright (c) 2013-2014 Jonathan Heilmann;
 *
 * CHANGELOG
+* 0.0.3:	-removed jQuery.noConflict() and added IIFE
+*			-added support for zepto
 * 0.0.2:	-added jpeg to filter
 * 0.0.1:	-initial release
 *
 */
-jQuery.noConflict();
-(function($) {
+;(function($) {
 	$.extend($.expr[':'], {
 		isImageFile: function(obj){
 			var $this = $(obj);
@@ -23,11 +24,11 @@ jQuery.noConflict();
 				case 'gif':
 					// Got an image - return true
 					return true;
-				break;
+					break;
 				default:
 					// No image found - return false
-				return false;
+					return false;
 			}
 		}
 	});
-})(jQuery);
+})(window.jQuery || window.Zepto);
